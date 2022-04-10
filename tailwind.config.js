9bcd3c410857f97aee2dc866a31cb34d84b594ef
Liana Bisuna <lianabisuna@gmail.com>
2022-04-10 15:38:55 +0800

    Import google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: ["Archivo", "sans-serif"],
+        narrow: ["Archivo Narrow", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
